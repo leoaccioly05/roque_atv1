abfff2bd08fa3f6073aad68e8858f6fd849dfb5e
leoaccioly05 <162645752+leoaccioly05@users.noreply.github.com>
2024-03-29 02:52:07 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,5 @@
+var numero1 = prompt("Por favor, digite o primeiro número:");
+var numero2 = prompt("Por favor, digite o segundo número:");
+var numero3 = prompt("Por favor, digite o terceiro número:");
+
+alert("Os números que você digitou são: " + numero1 + ", " + numero2 + ", " + numero3);
